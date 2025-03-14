0f8f83400b8ef26f7736a4dd48b4a2ab2e18c04c
miscakarza <miscakarza@gmail.com>
2025-03-14 19:57:49 +0700

    improve navbar css

--- views/src/components/Navbar.jsx
+++ views/src/components/Navbar.jsx
@@ -1,21 +1,61 @@
-//views\src\components\Navbar.jsx
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <h1>Kylong Coffee</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <h1>Kylong Coffee</h1>
+        </Link>
+
+        {/* Mobile menu button */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div className={`hamburger ${isMenuOpen ? "active" : ""}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        {/* Navigation menu */}
+        <ul className={`nav-menu ${isMenuOpen ? "active" : ""}`}>
+          <li className="nav-item">
+            <Link
+              to="/"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className="nav-link"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
